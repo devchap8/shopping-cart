ce4f828c7173f83b9aefdfae755b12f7d1209bc9
devchap8 <62908499+devchap8@users.noreply.github.com>
2026-06-15 13:23:04 -0400

    Import svgs

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,11 +1,15 @@
+import homeSvg from "./assets/home.svg";
+import storeSvg from "./assets/store.svg";
+import cartSvg from "./assets/cart.svg";
+
 export default function NavBar() {
     return (
         <nav className={"flex h-20 p-8 items-center bg-amber-400"}>
             <div className={"text-4xl font-bold"}>Devbay</div>
             <div className={"flex gap-4 ml-auto"}>
-                <div>Home Icon</div>
-                <div>Shop Icon</div>
-                <div>Cart Icon</div>
+                <img className={"w-12"} src={homeSvg}></img>
+                <img className={"w-12"} src={storeSvg}></img>
+                <img className={"w-12"} src={cartSvg}></img>
             </div>
         </nav>
     )
