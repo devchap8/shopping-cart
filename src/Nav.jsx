e5432059e26264bc31206d1db48ed62e830389de
devchap8 <62908499+devchap8@users.noreply.github.com>
2026-06-16 09:13:17 -0400

    Stylize header

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -5,12 +5,12 @@ import { Link } from "react-router";
 
 export default function NavBar() {
     return (
-        <nav className={"flex h-20 p-8 items-center bg-amber-400"}>
-            <div className={"text-4xl font-bold"}>Devbay</div>
+        <nav className={"flex h-20 p-8 items-center bg-brand-main text-ui-white"}>
+            <div className={"text-4xl font-bold tracking-wide"}>Devbay</div>
             <div className={"flex gap-4 ml-auto"}>
-                <Link to="/"><img className={"w-12"} src={homeSvg}></img></Link>
-                <Link to="store"><img className={"w-12"} src={storeSvg}></img></Link>
-                <Link to="cart"><img className={"w-12"} src={cartSvg}></img></Link>
+                <Link to="/"    > <img className={"w-12 hover:scale-115 ease-in-out duration-300 invert"} src={homeSvg}></img></Link>
+                <Link to="store"> <img className={"w-12 hover:scale-115 ease-in-out duration-300 invert"} src={storeSvg}></img></Link>
+                <Link to="cart" > <img className={"w-12 hover:scale-115 ease-in-out duration-300 invert"} src={cartSvg}></img></Link>
             </div>
         </nav>
     )
