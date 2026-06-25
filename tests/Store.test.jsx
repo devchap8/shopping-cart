fe9ed29d5c84e2c39c432e4020d13cbfe81d9e02
devchap8 <62908499+devchap8@users.noreply.github.com>
2026-06-25 10:17:24 -0400

    Finish testing shop

--- tests/Store.test.jsx
+++ tests/Store.test.jsx
@@ -80,5 +80,58 @@ it("sorts items when option selected", async () => {
     expect(itemList[0].innerHTML).toBe("Television");
     expect(itemList[35].innerHTML).toBe("Luxuary Tent");
     expect(itemList.length).toBe(36);
+});
+
+it("all elements work in tandem", async () => {
+    setup();
+    const user = userEvent.setup();
+    const searchInput = screen.getByRole('searchbox');
+    const selectFilter = screen.getByTestId("select-filter");
+    const selectSort = screen.getByTestId("select-sort");
+
+    await userEvent.selectOptions(selectFilter, "clothing");
+    let itemList = screen.getAllByTestId("item-name");
+    expect(itemList.length).toBe(9);
+    expect(itemList[0].innerHTML).toBe("Cotton Hoodie");
+
+    await userEvent.selectOptions(selectSort, "highest");
+    itemList = screen.getAllByTestId("item-name");
+    expect(itemList.length).toBe(9);
+    expect(itemList[0].innerHTML).toBe("Waterproof Raincoat");
+    expect(itemList[8].innerHTML).toBe("Knitted Wool Beanie");
+
+    await user.type(searchInput, "to");
+    itemList = screen.getAllByTestId("item-name");
+    expect(itemList.length).toBe(3);
+    expect(itemList[0].innerHTML).toBe("High-Top Sneakers");
+
+    await userEvent.selectOptions(selectSort, "lowest");
+    itemList = screen.getAllByTestId("item-name");
+    expect(itemList.length).toBe(3);
+    expect(itemList[0].innerHTML).toBe("Button-Down Shirt");
+
+    await userEvent.selectOptions(selectFilter, "lifestyle");
+    itemList = screen.getAllByTestId("item-name");
+    expect(itemList.length).toBe(2);
+    expect(itemList[0].innerHTML).toBe("Canvas Tote Bag");
+
+    await user.type(searchInput, "ooo");
+    itemList = screen.queryAllByTestId("item-name");
+    expect(itemList.length).toBe(0);
+
+    await userEvent.clear(searchInput);
+    itemList = screen.getAllByTestId("item-name");
+    expect(itemList.length).toBe(9);
+    expect(itemList[0].innerHTML).toBe("Canvas Tote Bag");
+
+    await userEvent.selectOptions(selectFilter, "all");
+    itemList = screen.getAllByTestId("item-name");
+    expect(itemList.length).toBe(36);
+    expect(itemList[itemList.length - 1].innerHTML).toBe("Ultra-Wide Monitor");
+
+    await userEvent.selectOptions(selectSort, "category");
+    itemList = screen.getAllByTestId("item-name");
+    expect(itemList.length).toBe(36);
+    expect(itemList[0].innerHTML).toBe("Television");
 })
 
