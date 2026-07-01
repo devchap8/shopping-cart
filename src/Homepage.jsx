06530145deb0793bbbb512fd609900393bed702f
devchap8 <62908499+devchap8@users.noreply.github.com>
2026-07-01 08:37:54 -0400

    Fix link in homepage

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -29,7 +29,7 @@ export default function Homepage() {
                 <div className={"flex flex-col justify-center gap-5 mb-10"}>
                     <div className={"font-bold text-3xl sm:text-5xl lg:text-6xl xl:text-7xl tracking-wide text-shadow-lime-300 text-shadow-lg"}>Summer Clothes Sale</div>
                     <div className={"text-sm sm:text-lg"}>Save 20% on clothes while offers apply</div>
-                    <button className={"self-start bg-lime-500 px-3 py-2 text-xl rounded-2xl font-bold text-white cursor-pointer transition-all duration-200 hover:bg-lime-300"}>Shop Now</button>
+                    <Link to="store" onClick={() => cont.filterByCat("clothing")} className={"self-start bg-lime-500 px-3 py-2 text-xl rounded-2xl font-bold text-white cursor-pointer transition-all duration-200 hover:bg-lime-300"}>Shop Now</Link>
                 </div>
                 <img className={"hidden md:block max-w-7/12 h-auto rounded-2xl"} src={clothesImg} alt="4 people modelling clothes"></img>
                 <img className={"md:hidden max-w-5/12 rounded-2xl"} src={clothesCropped} alt="4 people modelling clothes"></img>
